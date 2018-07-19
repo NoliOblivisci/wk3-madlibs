9d019d2df1d01e2c4b196708f2ec3f22a0fecfc7
Miles Dei <amicusiesu@gmail.com>
2018-07-19 17:47:43 +0000

    2 way binding with inputs on a card

--- src/components/card.js
+++ src/components/card.js
@@ -12,10 +12,12 @@ class Card extends Component {
             pluralNoun: '',
             number: ''
         }
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
     
-    handleInputChange() {
-        this.setState({ color: 'red'})
+    handleInputChange(event) {
+        this.setState({ color: event.target.value })
     }
     
     
@@ -24,7 +26,7 @@ class Card extends Component {
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input/>
+                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
             </div>
             
         )
