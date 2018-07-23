dd83a6e1b1a72a554282a7c44f069713931cfdd0
Miles Dei <amicusiesu@gmail.com>
2018-07-23 14:54:40 +0000

    modified handleInputChange to handle multiple input cases instead of just color

--- src/components/card.js
+++ src/components/card.js
@@ -8,16 +8,15 @@ class Card extends Component {
         super()
         
         this.state = {
-            color: 'BLUE',
-            pluralNoun: '',
-            number: ''
+            color: '',
+            pluralNoun: ''
         }
         
         this.handleInputChange = this.handleInputChange.bind(this);
     }
     
     handleInputChange(event) {
-        this.setState({ color: event.target.value })
+        this.setState({ [event.target.name]: event.target.value })
     }
     
     
@@ -26,18 +25,10 @@ class Card extends Component {
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
+                { Input('Color', this.state.color, this.handleInputChange, 'color' ) }
+                { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
             </div>
             
-        )
-        
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                { Input('Color') }
-                { Input('Plural Noun') }
-                { Input('Number') }
-            </div>
         )
     }
 }
